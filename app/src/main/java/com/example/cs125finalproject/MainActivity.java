6d1c6d3292a522ea099bd1ff039a3f5d134d21db
Ansh Bhalla <anshb2@illinois.edu>
2019-12-01 14:38:57 -0800

    Test Push

--- app/src/main/java/com/example/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/cs125finalproject/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.cs125finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView box;
+
+    private FrameLayout frame;
+
+    private Drawable imageBoxRight;
+    private Drawable imageBoxLeft;
+
+    private float boxX;
+    private float boxY;
+
+    private boolean action_up;
+    private boolean action_down;
+
+    private Timer timer = new Timer();
+
+    private Handler handler = new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        box = findViewById(R.id.box);
+        frame = findViewById(R.id.frame);
+        imageBoxLeft = getResources().getDrawable((R.drawable.box_left));
+
     }
 }
